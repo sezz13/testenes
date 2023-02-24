360b585b74e64d2fe34fdcf29131c96afa90179b
sezz13 <81531665+sezz13@users.noreply.github.com>
2023-02-24 17:28:46 +0300

    ilk yorum

---
+++ MerhabaDunya/MerhabaDunyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerhabaDunyaApp.swift
+//  MerhabaDunya
+//
+//  Created by Elif Aynur Kahraman on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MerhabaDunyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
